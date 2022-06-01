76766e0c1d4f8196b3459a6a6cdcd4dc4a84bacc
mira <nkskjjbb@gmail.com>
2022-06-02 00:28:49 +0900

    JPA Programing 8.JPA Principle

--- src/main/java/com/example/demo/JpaRunner.java
+++ src/main/java/com/example/demo/JpaRunner.java
@@ -1,47 +1,37 @@
 package com.example.demo;
 
-import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
-
 @Component
 @Transactional
 public class JpaRunner implements ApplicationRunner {
 
-	@PersistenceContext
-	EntityManager entityManager;
-
+	
+	@Autowired
+	PostRepository  postRepo;
+	
+	
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 
-		/* JPQL (HQL) */
-	//	TypedQuery<Post> query = entityManager.createQuery("Select b from Post as b", Post.class);
-	//	List<Post> result = query.getResultList();
-	//	result.forEach(System.out::println);
+//		Post post = new Post();
+//		post.setTitle("파라미터 찍히는가?");
+//		
+//		Comment comment = new Comment();
+//		comment.setComment("코멘트도저장되는가");
+//		
+//		post.addComment(comment);
+//		postRepo.save(post);
+//		
 		
 		
-		/* Criteria */
-//		CriteriaBuilder builder = entityManager.getCriteriaBuilder();
-//		CriteriaQuery<Post> criteria = builder.createQuery(Post.class);
-//		Root<Post> root = criteria.from(Post.class);
-//		criteria.select(root);
-//		List<Post> result = entityManager.createQuery(criteria).getResultList();
 		
+		postRepo.findAll().forEach(System.out::println);
 		
-		/* Native Query */
-		List<Post> result = entityManager.createNativeQuery("select * from Post", Post.class).getResultList();
-		result.forEach(System.out::println);
 	}
 
 }
