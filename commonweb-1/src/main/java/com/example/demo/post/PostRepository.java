0cde2c5e7985ae742b9fa9bd03a7f07836459982
mira <nkskjjbb@gmail.com>
2022-06-16 08:43:42 +0900

    JPA Programing SpringData 5. Named Paramete

--- commonweb-1/src/main/java/com/example/demo/post/PostRepository.java
+++ commonweb-1/src/main/java/com/example/demo/post/PostRepository.java
@@ -5,14 +5,15 @@ import java.util.List;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
 	List<Post> findByTitleStartsWith(String string);
 
 	
-	@Query("SELECT p FROM Post as p WHERE p.title = ?1")
-	List<Post> findByTitle(String string, Sort sort);
+	@Query("SELECT p FROM #{#entityName} as p WHERE p.title = :title")
+	List<Post> findByTitle(@Param("title") String keyword, Sort sort);
 
 	
 }
